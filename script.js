f3c95486f80c73d1748ffacfe43f2412488cff12
zul'jin <53787083+6mahdapihka9@users.noreply.github.com>
2020-02-28 08:43:49 +0200

    script update

--- script.js
+++ script.js
@@ -1,68 +1,43 @@
 //window.onunload(confirm('Ви дійсно хочете покинути сайт?'));
 //window.onload(areYouHere);
+function hideAll(){
+    document.getElementById("mainSection").hidden = true;
+    document.getElementById("menuSection").hidden = true;
+    document.getElementById("newsSection").hidden = true;
+    document.getElementById("inputSection").hidden = true;
+    document.getElementById("outputSection").hidden = true;
+    document.getElementById("authorSection").hidden = true;
+    document.getElementById("searchSection").hidden = true;
+}
 function hideSection(text){
     if (text === 'main') {
+        hideAll();
         document.getElementById("mainSection").hidden = false;
-        document.getElementById("menuSection").hidden = true;
-        document.getElementById("newsSection").hidden = true;
-        document.getElementById("inputSection").hidden = true;
-        document.getElementById("outputSection").hidden = true;
-        document.getElementById("authorSection").hidden = true;
-        document.getElementById("searchSection").hidden = true;
         document.title = "Головна";
     } else if (text === 'menu') {
-        document.getElementById("mainSection").hidden=true;
+        hideAll();
         document.getElementById("menuSection").hidden=false;
-        document.getElementById("newsSection").hidden=true;
-        document.getElementById("inputSection").hidden=true;
-        document.getElementById("outputSection").hidden=true;
-        document.getElementById("authorSection").hidden=true;
-        document.getElementById("searchSection").hidden=true;
         document.title = "Меню";
     } else if (text === 'news') {
-        document.getElementById("mainSection").hidden=true;
-        document.getElementById("menuSection").hidden=true;
+        hideAll();
         document.getElementById("newsSection").hidden=false;
-        document.getElementById("inputSection").hidden=true;
-        document.getElementById("outputSection").hidden=true;
-        document.getElementById("authorSection").hidden=true;
-        document.getElementById("searchSection").hidden=true;
         document.title = "Новини";
     } else if (text === 'input') {
-        document.getElementById("mainSection").hidden=true;
-        document.getElementById("menuSection").hidden=true;
-        document.getElementById("newsSection").hidden=true;
+        hideAll();
         document.getElementById("inputSection").hidden=false;
-        document.getElementById("outputSection").hidden=true;
-        document.getElementById("authorSection").hidden=true;
-        document.getElementById("searchSection").hidden=true;
         document.title = "Ввід інформації";
     } else if (text === 'output') {
-        document.getElementById("mainSection").hidden=true;
-        document.getElementById("menuSection").hidden=true;
-        document.getElementById("newsSection").hidden=true;
-        document.getElementById("inputSection").hidden=true;
+        hideAll();
         document.getElementById("outputSection").hidden=false;
-        document.getElementById("authorSection").hidden=true;
-        document.getElementById("searchSection").hidden=true;
         document.title = "Вивід інформації";
     } else if (text === 'author') {
-        document.getElementById("mainSection").hidden=true;
-        document.getElementById("menuSection").hidden=true;
-        document.getElementById("newsSection").hidden=true;
-        document.getElementById("inputSection").hidden=true;
-        document.getElementById("outputSection").hidden=true;
+        hideAll();
         document.getElementById("authorSection").hidden=false;
-        document.getElementById("searchSection").hidden=true;
         document.title = "Про автора";
     } else if (text === 'search') {
-        document.getElementById("mainSection").hidden=true;
-        document.getElementById("menuSection").hidden=true;
-        document.getElementById("newsSection").hidden=true;
-        document.getElementById("inputSection").hidden=true;
-        document.getElementById("outputSection").hidden=true;
-        document.getElementById("authorSection").hidden=true;
+        hideAll();
         document.getElementById("searchSection").hidden=false;
+        document.title = "Пошук";
     }
 }
 function chBGH1back() { document.getElementById('site_title').style.backgroundColor='transparent'; }
@@ -78,23 +53,30 @@ function areYouHere() {
 function showError(){
     alert("Приносимо свої вибачення. Ця сторінка ще не допрацьована!");
 }
-function searchWords(){
+function searchWords(someText){
+    hideAll();
+    document.getElementById("searchSection").hidden=false;
     let allText = document.getElementsByClassName("left");
-    let newArrayOfWords;
-    //for (let i = 0; i < allText.length; i++) {
     //newArrayOfWords = allText[0].textContent.split(" ");
-    let found;
+    let amountOfWords, newArray, finalArray;
     let str = "aaaa bbbbA ddd Baaa";
-    let reg1 = /^н/g;
     let reg2 = /a[a-z]a/g;
+    let reg1 = /^н/g;
     let reg3 = /(\bн)(и\b)/g;
     let regForWords = /[.,\/ -]/;
     let regForWords1 = /[ ,.:;-?!]+/;
-    newArrayOfWords = allText[0].textContent.split(regForWords1);
-    //found = str.match(reg2);
-    found = allText[0].textContent.match(reg1);
+    newArray = allText[0].textContent.split(regForWords1);
+    finalArray = allText[0].textContent.match(reg1);
 
-    console.log( newArrayOfWords );
+    console.log( someText );
+    let searchBlock = document.getElementById("searchSection");
+    searchBlock.removeChild()
+    let searchP = document.createElement('p');
+    searchP.id="searchParagraph";
+    for (let i = 0; i < newArray.length; i++) {
+        searchP.innerHTML = "" + newArray[i] + ", ";
+    }
+    searchBlock.append(searchP);
 }
 
 
